afc01eb7bb4c69e1b4896b0e512a44bdb1302279
md-wasik-alam <wasikalam02@gmail.com>
2022-10-19 09:50:41 +0530

    d

--- src/App.js
+++ src/App.js
@@ -9,7 +9,30 @@ import Table from './components/Table';
 function App() {
 
 
- 
+  // const [todos, setTodos] = useState([]);
+  // useEffect(() => {
+  //   const q = query(collection(db, "todos"));
+  //   const unsub = onSnapshot(q, (QuerySnapshot) => {
+  //     let todoArray = [];
+  //     QuerySnapshot.forEach((doc) => {
+  //       todoArray.push({ ...doc.data(), id: doc.id })
+  //     })
+  //     setTodos(todoArray);
+  //   })
+  //   return () => unsub()
+  // }, [])
+
+  // const handleDelete = async (id) => {
+  //   await deleteDoc(doc(db,"todos",id));
+  // }
+  // const handleDone = async (todo) => {
+  //     await updateDoc(doc(db,"todos",todo.id),{
+  //       completed:!todo.completed
+  //     })
+  // }
+  // const handleEdit = async (todo, title) => {
+  //   await updateDoc(doc(db, "todos", todo.id), { title: title })
+  // }
 
 
   return (
